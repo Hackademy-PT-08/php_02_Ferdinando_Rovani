a97c8ddf94bc8f8702754b1f0864eb26bc4211ef
Frovani91 <ferdinando.rovani@live.it>
2023-06-29 19:21:14 +0200

    esrcizio3Esercitazione2

--- index.php
+++ index.php
@@ -55,3 +55,20 @@ $media = $totalePari/$contatorePari;
 echo($media);
 
 
+// Scrivere un programma che stampi in console tutti i numeri da uno a cento. Se il numero è multiplo di 3 stampare “PHP” al posto del numero; se multiplo di 5 stampare “JAVASCRIPT”; se multiplo di 3 e 5 contemporaneamente deve stampare “HACKADEMY".
+
+for($i=0; $i<=100; $i++){
+    if($i%3===0 && $i%5===0){
+        echo("HACKADEMY \n");
+    }
+    elseif($i%3===0){
+        echo("PHP \n");
+    }
+    elseif($i%5===0){
+        echo("JAVASCRIPTPHP \n");
+    }
+    else{
+        echo("$i \n");
+    }
+}
+
